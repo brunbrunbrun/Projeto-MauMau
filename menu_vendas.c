67bb80410b58f7261bc4d214cad4b928b723b01a
Brunu <comachiobelobruno@protonmail.com>
2022-07-05 21:06:20 -0400

    esboco da nova_venda()

--- menu_vendas.c
+++ menu_vendas.c
@@ -43,47 +43,105 @@ void nova_venda()
 
 
 
-
-
     printf("\t\t-------------------------\n");
     printf("\t\tno formato 123456789-01\n");
     printf("\t\tInforme o CPF do cliente: ");
     scanf(" %[^\n]s",CPF);
 
     //olha se existe, caso sim, retorna um int igual ou maior que 0
-    int index = (tem_cpf(CPF));
+    int index_cliente = (tem_cpf(CPF));
 
-    if(index < 0)
+    if(index_cliente < 0)
     {//Não existe, pede pra cadastrar
         printf("\t\tCPF nao cadastrado, prosseguindo para processo de cadastro\n");
         cadastro_cliente();
         //apos cadastro, procura novamente o index de onde esta o cpf no arquivo
-        index = (tem_cpf(CPF));
+        index_cliente = (tem_cpf(CPF));
     }
+
      //prossegue com a venda, sabendo que o cliente esta cadastrado, e onde.
-    else
-    {
-        FILE *f = fopen("./Arquivos/produtos.dat","ab");
-        TProduto prod;
 
-        int quant;
+     bool ainda = true;
 
-        printf("Informe o ID do produto que deseja comprar: ");
-        scanf(" %d", &prod.ID_Produto);
+     while(ainda == true)
+     {
+         //variaveis relacionadas aos produtos
+         int ID_do_produto;
+         TProduto prod;
+         FILE *f_prod = fopen("./Arquivos/produtos.dat","rb+");
+         bool achou = false;
+         int index_produto = 0;
+         bool existe;
 
-        if(){
-            printf("Informe a quantidade do produto: ");
-            scanf(" %d", &quant);
-        }
-        else
-        {
-            printf("ID inválido, tente novamente");
-        }
+         printf("\t\tDigite o ID do produto: ");
+         scanf(" %d", &ID_do_produto);
 
-      fwrite(&prod,sizeof(TProduto),1,f);
-      fclose(f);
+         //achar o index de onde esta o produto
+         while(achou == false)
+         {
+             fread(&prod,sizeof(TProduto),1,f_prod);
 
-    }
+             if(prod.ID_Produto == ID_do_produto)
+             {
+                 achou = true;
+                 existe = true;
+             }
+             else
+             {
+                 index_produto++;
+             }
+
+             if(feof(f_prod))
+             {
+                 existe = false;
+                 break;
+             }
+         }
+         //nao achou o produto procurando por id, possivelmente id foi digitado
+         //errado ou algo assim
+         if(existe == false)
+         {
+             printf("\t\tNao foi possivel localizar o produto utilizando o ID informado\n");
+
+         }
+
+         else
+         {
+
+             fseek(f_prod,index_produto * sizeof(TProduto),SEEK_SET);
+             fread(&prod,sizeof(TProduto),1,f_prod);
+
+             printf("\t\tNome: %s\t\tID: %d\n",prod.Nome,prod.ID_Produto);
+             printf("\t\tPreco R$:%.2f\n",prod.Preco);
+             printf("\t\tEstoque: %d\n",prod.Estoque);
+
+             //caso esteja fora de estoque, a compra do produto atual é anulada,
+             //e pulamos para o fim do loop onde pergunta se quer continuar
+             if(prod.Estoque <= 0)
+             {
+                 printf("\t\tPRODUTO FORA DE ESTOQUE\n");
+                 printf("\t\tCOMPRA ANULADA\n");
+             }
+             else
+             {
+                 //Estoque > 0 então continuamos com a venda
+
+
+             }
+         }
+
+            //sair ou continuar no loop de listagem de produtos
+         int sim_nao;
+         printf("\t\tDeseja continuar a venda?\n");
+         printf("\t\t1. Sim\n\t\t2. Nao (voltar)\n\t\t");
+         scanf(" %d",&sim_nao);
+
+         if(sim_nao == 2)
+         {
+             ainda = false;
+         }
+
+     }
 
 }
 
@@ -109,7 +167,6 @@ void listar_compras_cliente()
     {
         case 1: printf("\t\t\tInsira o CPF: ");
                 scanf(" %[^\n]s",CPF);
-                if (CPF)
         break;
 
         case 2: printf("\t\t\tInsira o Nome Completo: ");
@@ -127,7 +184,7 @@ void listar_compras_cliente()
         break;
     }
 
-    
+
 
 
 
